5b72917f060e756ee9e77631d3b0d4379a758582
thesawisthelaw <dimakrok@bk.ru>
2022-11-13 23:27:32 +0300

    FIX: 1000000000%

--- src/payment/src/main/kotlin/services/payment/controller/PaymentController.kt
+++ src/payment/src/main/kotlin/services/payment/controller/PaymentController.kt
@@ -11,22 +11,22 @@ import java.util.*
 @RequestMapping("api/v1/payment")
 class PaymentController {
 
-//    @GetMapping("/{paymentUid}")
-//    fun getPayment(@PathVariable paymentUid: UUID): ResponseEntity<Payment> =
-//        ResponseEntity.ok(PaymentRepository.get(paymentUid))
-//
-//    @GetMapping("/")
-//    fun getPayments(): ResponseEntity<List<Payment>> =
-//        ResponseEntity.ok(PaymentRepository.get())
-//
-//    @PatchMapping("/{paymentUid}/cancel")
-//    fun cancelPayment(@PathVariable paymentUid: UUID): ResponseEntity<*> =
-//        ResponseEntity.ok(PaymentRepository.setStatus(paymentUid, "CANCELED"))
-//
-//    @PostMapping("/")
-//    fun addPayment(@RequestBody payment: Payment): ResponseEntity<Int> =
-//        ResponseEntity.ok(PaymentRepository.add(payment))
-//
-//    @GetMapping("/manage/health")
-//    fun healthCheck(): ResponseEntity<*> = ResponseEntity.ok(null)
+    @GetMapping("/{paymentUid}")
+    fun getPayment(@PathVariable paymentUid: UUID): ResponseEntity<Payment> =
+        ResponseEntity.ok(PaymentRepository.get(paymentUid))
+
+    @GetMapping("/")
+    fun getPayments(): ResponseEntity<List<Payment>> =
+        ResponseEntity.ok(PaymentRepository.get())
+
+    @PatchMapping("/{paymentUid}/cancel")
+    fun cancelPayment(@PathVariable paymentUid: UUID): ResponseEntity<*> =
+        ResponseEntity.ok(PaymentRepository.setStatus(paymentUid, "CANCELED"))
+
+    @PostMapping("/")
+    fun addPayment(@RequestBody payment: Payment): ResponseEntity<Int> =
+        ResponseEntity.ok(PaymentRepository.add(payment))
+
+    @GetMapping("/manage/health")
+    fun healthCheck(): ResponseEntity<*> = ResponseEntity.ok(null)
 }
